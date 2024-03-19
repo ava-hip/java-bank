c9e88e56992c9ba23e9426630975a3e5baa7505c
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-03-19 16:10:07 +0100

    Initial commit

---
+++ src/main/java/fr/hip/javabank/JavaBankApplication.java
@@ -0,0 +1,13 @@
+package fr.hip.javabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBankApplication.class, args);
+    }
+
+}
